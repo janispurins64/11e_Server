76332f91f513c7c2cf4f5ea1bef771820d7515c6
janispurins64 <janispurin64@gmail.com>
2023-02-01 21:37:09 +0200

    Dialogs
    Pievienotas lapas dialoga veidošanai, apmeklējuma skaitītāja sagatave.

--- galva.py
+++ galva.py
@@ -10,9 +10,21 @@ app = Flask(__name__)
 def root():
     return render_template("tests.html")
 
+@app.route('/uzruna')
+def uzruna():
+      
+      
+  return render_template("sveiciens.html",skaits="otrais")  
+  
+
+@app.route('/vards')
+def katevisauc():
+  return render_template("katevisauc.html")  
+  
+
 @app.route('/tests')
 def health():
   return render_template("tests.html")
 
 if __name__ == '__main__':
-  app.run(debug=True,host='10.1.15.44',port=5000) # host='0.0.0.0' - datora IP adrese
+  app.run(debug=True,port=5000) # ,host='10.1.15.44' host='0.0.0.0' - datora IP adrese
